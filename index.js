5f76b49f100819e630e5b0995f5db8f90569a4ad
PublicoOne <120681199+PublicoOne@users.noreply.github.com>
2024-06-24 14:02:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello worlderr');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Puerto 3000 como valor por defecto
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Aplicación escuchando en el puerto ${port}`);
+});
